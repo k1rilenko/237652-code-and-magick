1612f3f86c863057a31d66aed8a842aba3c6b236
Roman Kirilenko <cap.spouk@gmail.com>
2018-09-18 15:04:51 +0300

    Почти все сделал, остался вопрос с закрытием окна по кнопке ESC когда активен фокус

--- js/setup.js
+++ js/setup.js
@@ -1,6 +1,7 @@
 'use strict';
+var KEY_ESC = 27;
+var KEY_ENTER = 13;
 var gameSetup = document.querySelector('.setup');
-gameSetup.classList.remove('hidden');
 var wizardSetup = document.querySelector('.setup-similar');
 wizardSetup.classList.remove('hidden');
 var wizardList = document.querySelector('.setup-similar-list');
@@ -36,3 +37,69 @@ for (var i = 0; i < 4; i++) {
   fragment.appendChild(wizardElement);
 }
 wizardList.appendChild(fragment);
+
+var setupOpen = document.querySelector('.setup-open-icon');
+var setupClose = gameSetup.querySelector('.setup-close');
+var setupUserName = gameSetup.querySelector('.setup-user-name');
+
+var popupEscPressHandler = function (evt) {
+  if (evt.keyCode === KEY_ESC) {
+    closePopup;
+  }
+};
+
+var openPopup = function () {
+  gameSetup.classList.remove('hidden');
+  document.addEventListener('keydown', function (evt) {
+    if (evt.keyCode === KEY_ESC) {
+      gameSetup.classList.add('hidden');
+    }
+  });
+};
+
+var closePopup = function () {
+  gameSetup.classList.add('hidden');
+  document.removeEventListener('keydown', popupEscPressHandler);
+};
+
+
+setupOpen.addEventListener('click', function () {
+  openPopup();
+});
+
+setupOpen.addEventListener('keydown', function (evt) {
+  if (evt.keyCode === KEY_ENTER) {
+    openPopup();
+  }
+});
+setupClose.addEventListener('click', function () {
+  closePopup();
+});
+setupClose.addEventListener('keydown', function (evt) {
+  if (evt.keyCode === KEY_ENTER) {
+    closePopup();
+  }
+});
+var setupWizard = document.querySelector('.setup-wizard');
+var inputCoatColor = document.querySelector('input[name=coat-color]');
+var inputEyesColor = document.querySelector('input[name=eyes-color]');
+var inputFireballColor = document.querySelector('input[name=fireball-color]');
+var getWizardColor = function (wizElem, arr, elem) {
+  var randomValue = getRandomValue(arr);
+  setupWizard.querySelector(wizElem).style.fill = randomValue;
+  elem.value = randomValue;
+};
+setupWizard.querySelector('.wizard-coat').addEventListener('click', function () {
+  getWizardColor('.wizard-coat', coatColors, inputCoatColor);
+});
+setupWizard.querySelector('.wizard-eyes').addEventListener('click', function () {
+  getWizardColor('.wizard-eyes', eyesColors, inputEyesColor);
+});
+
+var fireballsColor = ['#ee4830', '#30a8ee', '#5ce6c0', '#e848d5', '#e6e848'];
+var wizardFireball = document.querySelector('.setup-fireball-wrap');
+wizardFireball.addEventListener('click', function () {
+  var randomColorFireball = getRandomValue(fireballsColor);
+  wizardFireball.style.background = randomColorFireball;
+  inputFireballColor.value = randomColorFireball;
+});
